426b2ae05f2500871995c140e8f8c7163595178e
Corbs <corbsmartin@pivotal.io>
2018-07-24 22:34:55 -0500

    adding tags column

--- src/main/kotlin/io/corbs/todos/TodoEntity.kt
+++ src/main/kotlin/io/corbs/todos/TodoEntity.kt
@@ -4,9 +4,9 @@ import javax.persistence.*
 
 @Entity
 @Table(name="todos")
-data class TodoEntity(val title: String, val completed: Boolean) {
+data class TodoEntity(val title: String, val tags: String, val completed: Boolean) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Int? = null
-    constructor() : this("",false)
+    constructor() : this("","",false)
 }
